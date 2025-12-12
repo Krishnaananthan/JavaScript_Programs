bfb2bae619bc597feab10d9e138d85a27d0e1aa2
Krishna Anandhan <146909854+Krishnaananthan@users.noreply.github.com>
2025-12-12 17:29:15 +0530

    Add files via upload

---
+++ Keys.js
@@ -0,0 +1,4 @@
+function keys(obj){
+    return Object.keys(obj);
+}
+console.log(keys({name:"Krishna",age:22,city:"Chennai"}))
---
+++ oddOrEven.js
@@ -0,0 +1,9 @@
+function OddOrEven(num){
+    if(num%2 == 0){
+        return "Even"
+    }
+    else{
+        return "Odd"
+    }
+}
+console.log(OddOrEven(6))
